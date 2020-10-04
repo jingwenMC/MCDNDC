05293d3580c25b8607936131bfc36600947e22a9
jingwenMC <2313055824@qq.com>
2020-10-04 10:49:57 +0800

    Non-Versioning Update

--- src/main/java/top/jingwenmc/mcdndc/commands/dndc/next.java
+++ src/main/java/top/jingwenmc/mcdndc/commands/dndc/next.java
@@ -1,12 +1,26 @@
 package top.jingwenmc.mcdndc.commands.dndc;
 
+import com.sun.deploy.security.SelectableSecurityManager;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import top.jingwenmc.mcdndc.Main;
+import top.jingwenmc.mcdndc.objects.GamePlayer;
 import top.jingwenmc.mcdndc.objects.JCommand;
+import top.jingwenmc.mcdndc.util.MessageUtil;
 
 public class next extends JCommand {
     @Override
     public boolean onCommand(String[] args, CommandSender sender) {
-        //TODO:onCommand
+        if(sender instanceof Player)
+        {
+            //Noting to do
+        }
+        else
+        {
+            MessageUtil.sendPlayer(sender,"server.not_player");
+            return false;
+        }
+        GamePlayer gp = Main.INSTANCE.getPlayerManager().getGamePlayer(sender.getName());
         return true;
     }
 }
