2aadb485f7893d49a8f204d8199cc015bbd70cc3
jingwenMC <2313055824@qq.com>
2020-11-07 15:21:49 +0800

    1.9.92-INDEV Development Update TAB API

--- src/main/java/top/jingwenmc/mcdndc/provider/TABProvider.java
+++ src/main/java/top/jingwenmc/mcdndc/provider/TABProvider.java
@@ -2,6 +2,7 @@ package top.jingwenmc.mcdndc.provider;
 
 import me.neznamy.tab.api.EnumProperty;
 import me.neznamy.tab.api.TABAPI;
+import me.neznamy.tab.api.TabPlayer;
 import org.bukkit.ChatColor;
 import top.jingwenmc.mcdndc.objects.GamePlayer;
 import top.jingwenmc.mcdndc.objects.MCDNDCProvider;
@@ -10,8 +11,9 @@ import top.jingwenmc.mcdndc.util.MessageUtil;
 public class TABProvider extends MCDNDCProvider {
     @Override
     public void onWordSet(GamePlayer gamePlayer, String word) {
-        TABAPI.setValueTemporarily(gamePlayer.getPlayer().getUniqueId(), EnumProperty.TAGPREFIX, ChatColor.GOLD+"["
-                +ChatColor.translateAlternateColorCodes('&',word)+ChatColor.GOLD+"]&r");
+        TabPlayer player = TABAPI.getPlayer(gamePlayer.getPlayer().getUniqueId());
+        player.setValueTemporarily(EnumProperty.TAGPREFIX, ChatColor.GOLD
+                +"[" +ChatColor.translateAlternateColorCodes('&',word)+ChatColor.GOLD+"]&r");
     }
 
     @Override
