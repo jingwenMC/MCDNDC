e71c90097c1a3dd97d78c8745d2e3fff66cfb829
jingwenMC <2313055824@qq.com>
2020-12-12 12:02:56 +0800

    1.9.93-SNAPSHOT Security Hotspot

--- src/main/java/top/jingwenmc/mcdndc/util/ExceptionUtil.java
+++ src/main/java/top/jingwenmc/mcdndc/util/ExceptionUtil.java
@@ -10,7 +10,9 @@ public class ExceptionUtil {
     public static void print(Throwable e) {
         File eFile = new File(Main.getInstance().getDataFolder(), "errors.txt");
         try {
-            if (!eFile.exists()) eFile.createNewFile();
+            if (!eFile.exists()) if(!eFile.createNewFile()) {
+                throw new IllegalStateException("Cannot Create File");
+            }
             if (!eFile.canWrite()) throw new IOException("errors.txt is not writable");
             FileWriter out = new FileWriter(eFile,true);
             PrintWriter writer = new PrintWriter(out);
@@ -23,12 +25,10 @@ public class ExceptionUtil {
             out.close();
             Main.getInstance().getLogger().log(Level.SEVERE, "一个错误已经发生,已将该错误信息保存到插件目录下的 errors.txt");
             Main.getInstance().getLogger().log(Level.SEVERE, "An error has occurred and the error message has been saved to errors.txt in the plugin directory");
-        } catch (IOException ioException) {
-            System.out.println("MCDNDC UNEXPECTED ERROR:Cannot Create/Write errors.txt While Recording An Error");
-            System.out.println("Printing Creating/Writing StackTrace...");
-            ioException.printStackTrace();
-            System.out.println("Printing StackTrace Directly...");
-            e.printStackTrace();
+        } catch (IOException | IllegalStateException exception) {
+            System.err.println("MCDNDC UNEXPECTED ERROR:Cannot Create/Write errors.txt While Recording An Error");
+            System.err.println("IF ITS ALL RIGHT, PLEASE REPORT TO DEV.");
+            System.err.println("Message:"+exception.getMessage());
         }
     }
 }
